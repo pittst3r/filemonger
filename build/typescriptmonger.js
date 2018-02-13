5aed6bf275f0d646dc3e6903fe8e9a96c197bdfa
Robbie Pitts <me@robbie-pitts.com>
2018-02-12 23:59:52 -0500

    Self-build

---
+++ build/typescriptmonger.js
@@ -0,0 +1,54 @@
+const makeFilemonger = require("filemonger").default;
+const helpers = require("filemonger").helpers;
+const ts = require("typescript");
+const path = require("path");
+
+const typescriptmonger = makeFilemonger((file$, { srcDir, destDir }) =>
+  file$
+    .map(file => path.join(srcDir, file))
+    .toArray()
+    .do(files => {
+      const tsConfig = require(path.join(process.cwd(), "tsconfig.json"));
+      const baseOptions = tsConfig.compilerOptions;
+      const compilerOptions = {
+        ...baseOptions,
+        rootDir: srcDir,
+        outDir: destDir
+      };
+      const program = ts.createProgram(files, compilerOptions);
+      const diagnostics = ts.getPreEmitDiagnostics(program);
+
+      handleDiagnostics(diagnostics);
+      program.emit();
+    })
+    .flatMapTo(helpers.filesInDir(destDir))
+);
+
+function handleDiagnostics(diagnostics) {
+  diagnostics.forEach(diagnostic => {
+    if (diagnostic.file) {
+      let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
+        diagnostic.start
+      );
+      let message = ts.flattenDiagnosticMessageText(
+        diagnostic.messageText,
+        "\n"
+      );
+      console.log(
+        `${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`
+      );
+    } else {
+      console.log(
+        `${ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n")}`
+      );
+    }
+  });
+
+  if (diagnostics.length > 0) {
+    throw new Error(
+      `TypeScript compilation failed with ${diagnostics.length} error(s)`
+    );
+  }
+}
+
+module.exports = typescriptmonger;
