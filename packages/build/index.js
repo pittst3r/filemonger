c7a6873917bdaa9cd0961169b2c7ffbe24752c47
Robbie Pitts <me@robbie-pitts.com>
2018-02-20 11:56:14 -0500

    Make CLI better

--- packages/build/index.js
+++ packages/build/index.js
@@ -1,19 +1,5 @@
 const { typescriptmonger } = require("@filemonger/typescriptmonger");
 
-module.exports = env => {
-  const args =
-    (process.env.NODE_ENV || env || "prod") === "test"
-      ? ["test/**/*.ts", "./", "./tmp"]
-      : ["index.ts", "./src", "./dist"];
-
-  console.log("Building...");
-  console.time("Build time");
-  typescriptmonger(args.shift()).run(...args, err => {
-    console.timeEnd("Build time");
-
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-  });
-};
+module.exports = typescriptmonger(
+  process.env.NODE_ENV === "test" ? "test/**/*.ts" : "index.ts"
+);
--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,7 +1,33 @@
 #!/usr/bin/env node
 
 const { join } = require("path");
+const yargs = require("yargs");
+
+const argv = yargs
+  .option("src", {
+    alias: "s",
+    describe: "Source directory to pull files from",
+    type: "string"
+  })
+  .option("dest", {
+    alias: "d",
+    describe: "Destination directory to place transformed files",
+    type: "string"
+  })
+  .demandOption(["src", "dest"]).argv;
+
+const srcDir = argv.src;
+const destDir = argv.dest;
 
 const mongerfile = require(join(process.cwd(), "mongerfile"));
 
-mongerfile();
+console.log("Filemongering...");
+console.time("Time");
+mongerfile.run(srcDir, destDir, (err, files) => {
+  console.timeEnd("Time");
+
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+});
